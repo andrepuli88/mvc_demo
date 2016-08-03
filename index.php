c760b668ccb8833bd258f77227f81393dd11a4a3
Andrea Puligheddu <Andrea Puligheddu>
2016-08-03 19:37:42 +0200

    update 3 agosto 16

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ require 'libs/Controller.php';
 require 'libs/Model.php';
 require 'libs/View.php';
 
+// Library
+require 'libs/Database.php';
+
 require 'config/paths.php';
 require 'config/database.php';
 
--- libs/Controller.php
+++ libs/Controller.php
@@ -7,4 +7,16 @@ class Controller {
 		$this->view = new View();
 	}
 
+        public function loadModel($name){
+                
+            $name = 'models/'.$name.'_model.php';
+            
+            if(file_exists($path)) {
+                require 'models/'.$name.'_model.php';
+                
+                $modelName = $name . '_Model';
+                $this->model = new $modelName();
+            }
+        }
+        
 }
